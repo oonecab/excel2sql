5da1a072546548b0e0e2ab85edb2d61c5f4954e4
ZZZping <475377765@qq.com>
2025-11-24 13:36:40 +0800

    fix: downgrade axios to fix jest esm issue

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders landing page logo', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByRole('heading', { name: '数据转 SQL 工具集' })).toBeInTheDocument();
+});
+
+test('renders get started button', () => {
+  render(<App />);
+  expect(screen.getByRole('button', { name: '开始使用' })).toBeInTheDocument();
 });
